de401c2bac1e60af7eebe155ed18867e00276caa
Marina Leon <marinaleon@MacBook-Pro-de-Marina.local>
2016-07-01 15:51:26 +0100

    App with node and angular
    Node and Angular app for create, list and edit contacts saving data in
    the local storage of the www browser. Uses foundation for styles
    (similar to bootstrap) and a node module called country-list. The
    server must run with node using “node app/server.js”

--- app/scripts/contacts.js
+++ app/scripts/contacts.js
@@ -109,7 +109,7 @@ angular.module('addressBook.contacts', ['ngRoute', 'ngDialog'])
 		$scope.email = contact.email;
 		$scope.country = contact.country;
 	}
-
+	//functions for email validation (called from addFormSubmit)
 	function validateEmail(email) {
 	  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	  return re.test(email);
